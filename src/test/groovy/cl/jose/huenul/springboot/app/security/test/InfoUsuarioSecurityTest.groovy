28655d1b2cab9155cb21bb4d3d962664ba1a421c
José Huenul Muñoz <jhuenulm@everis.com>
2020-08-27 01:49:12 -0400

    se agrega nuevo tests a package security

---
+++ src/test/groovy/cl/jose/huenul/springboot/app/security/test/InfoUsuarioSecurityTest.groovy
@@ -0,0 +1,24 @@
+package cl.jose.huenul.springboot.app.security.test
+
+import static org.junit.Assert.assertNotNull
+import static org.junit.jupiter.api.Assertions.*
+
+import cl.jose.huenul.springboot.app.entity.Usuario
+import cl.jose.huenul.springboot.app.security.InfoUsuarioSecurity
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+import spock.lang.Specification
+class InfoUsuarioSecurityTest extends Specification{
+	
+	def "Evaluar token NOTNULL"(){
+		given:
+			def	infoUsuarioSecurity = new InfoUsuarioSecurity()
+			def user = new Usuario(name: 'joseph', email: 'joseph@test.com')
+		when:
+			def valor = infoUsuarioSecurity.getJWTToken(user) 
+		then:
+			assertNotNull(valor)
+	}
+}
